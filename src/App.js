47258344cb8f48406b630df9537d3cfe2d116032
Brad Stevenson <bradjste@gmail.com>
2020-03-21 20:47:00 -0500

    update file for ci test

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import splashCard from './img/bsSplashCard.png'
 const background = require('./web2Back.png');
 
 class App extends Component {
-  x = 50;
-  y = 50;
 
   constructor(props) {
       super(props);
