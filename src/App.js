95f69ee167a6cae4d9525ecd6cfc01254f62dca8
Brad Stevenson <bradjste@gmail.com>
2020-04-12 22:51:45 -0500

    Impl of 'paint' style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import './App.css';
+import About from './pageComps/About'
+import Art from './pageComps/About'
+import Music from './pageComps/About'
+import Contact from './pageComps/About'
 import P5Sketch from './P5Sketch.js';
 import splashCard from './img/bsSplashCard.png'
 import ReactTypingEffect from 'react-typing-effect'
@@ -12,11 +17,14 @@ class App extends Component {
         rules: [1,0,1,1,0,1,1,0],
         hues: [0,30,60,90,120,150,180,210],
         sats: [100,100,100,100,20,30,40,50],
-        isBlur: true
+        fil: [],
+        isBlur: true,
+        boxNum: 250
       };
       this.changeBlur = this.changeBlur.bind(this)
       this.changePattern = this.changePattern.bind(this)
       // setInterval(this.changeBlur,10000)
+      this.changePattern()
   }
 
   changeBlur(e) {
@@ -24,7 +32,7 @@ class App extends Component {
       document.getElementById('LiveBackDrop').style.filter = 'blur(0px)';
     }
     else {
-      document.getElementById('LiveBackDrop').style.filter = 'blur(6px)';
+      document.getElementById('LiveBackDrop').style.filter = 'blur(4px)';
     }
     this.setState(
       prevState => ({
@@ -36,6 +44,14 @@ class App extends Component {
 
   onHover(e) {
 
+  }
+
+  randomizeFil() {
+    let filArr = [];
+    for (let i = 0; i< this.state.boxNum; i++) {
+      filArr[i] = Math.floor(Math.random()*2);
+    }
+    return filArr;
   }
 
   changePattern(e) {
@@ -51,6 +67,7 @@ class App extends Component {
         }
         return(
           {
+            fil: this.randomizeFil(),
             rules: ruleArr,
             hues: hueArr,
             sats: satArr
@@ -60,37 +77,64 @@ class App extends Component {
     )
   }
 
-  
-
-    
   render() {
-    const typeText = ['welcome to my website','it\'s not ready quite yet, sorry about that','feel free to click my name and generate some new patterns','you can reach me at bradjste@gmail.com in the meantime']
+    const typeText = ['welcome to my website',
+                      'it\'s not ready quite yet, sorry about that',
+                      'feel free to around and generate some new patterns',
+                      'you can reach me at bradjste@gmail.com in the meantime']
     return(
+      <Router>
+        
       <div  id='App'>          
 
-        <div id='LiveBackDrop' className="LiveBackDrop" onMouseMove={this.handleMouseMove}>
-          <P5Sketch canvasParentRef='LiveBackDrop' sats={this.state.sats} rules={this.state.rules} hues={this.state.hues}/>   
-        </div> 
-
-        <div id = 'UI' className="UI">
-          {/* <button onClick={this.changeBlur} onHover={this.onHover}>
-            Blur
-          </button>  
-          <button onClick={this.changePattern}>
-            Pattern
-          </button>   */}
-          <div className="splashCard"> 
-            <img src={splashCard} onClick={this.changePattern} alt="title card of site" />
-            
-          </div>
-          <div id = 'construct' className="construct" onClick={this.changeBlur}>
-          <ReactTypingEffect className="constructR" text={typeText} speed={50} eraseDelay={800}/>
-            {/* <h1>New site under construction!</h1> */}
-          </div>
+        <div id='LiveBackDrop' className="LiveBackDrop" onMouseMove={this.handleMouseMove}  onClick={this.changePattern}>
+          <P5Sketch canvasParentRef='LiveBackDrop' 
+              sats={this.state.sats} 
+              rules={this.state.rules} 
+              hues={this.state.hues} 
+              boxNum={this.state.boxNum}
+              fil={this.state.fil}/>   
         </div>  
+        
+        <Route exact path="/">
+          <div id = 'UI' className="UI">
+
+            {/* 
+            <div className="linkDiv">
+            <Link to="/about" ><button>About</button></Link>
+            <Link to="/art" ><button>Art</button></Link>
+            <Link to="/music" ><button>Music</button></Link>
+            <Link to="/contact"><button>Contact</button></Link>
+            </div> 
+            */}
+
+            <div className="splashCard"> 
+              <img src={splashCard} onClick={this.changeBlur} alt="title card of site" />
+            </div>
+
+            <div id = 'construct' className="construct" >
+            <ReactTypingEffect className="constructR" text={typeText} speed={50} eraseDelay={800}/>
+              {/* <h1>New site under construction!</h1> */}
+              
+            </div>
+          </div>
+        </Route> 
+        <Route path="/about">
+          <About />
+        </Route> 
+        <Route path="/art">
+          <Art />
+        </Route> 
+        <Route path="/music">
+          <Music />
+        </Route> 
+        <Route path="/contact">
+          <Contact />
+        </Route> 
         
 
       </div>
+      </Router>
     );
   }
 }
--- src/P5Sketch.js
+++ src/P5Sketch.js
@@ -2,23 +2,19 @@ import React, {Component} from 'react'
 import Sketch from 'react-p5'
 
 let randHue = Math.floor(Math.random()*360)
-// let stretch = Math.random()*2-1
 let phase = 0;
-let circBool = false;
+let mouseIsPressed = false;
+let circBool = true;
+let lastRules = []
 class P5Sketch extends Component {
 
   
   constructor(props) {
     super(props)
     this.state = {
-      rules: this.props.rules,
-      hues: this.props.hues,
-      sats: this.props.sats,
-      fil: [],
-      boxNum: 75
+      fil: []
     }
     this.initFil = this.initFil.bind(this)
-    this.initRules = this.initRules.bind(this)
     this.calcNil = this.calcNil.bind(this)
   }
 
@@ -30,21 +26,22 @@ class P5Sketch extends Component {
     this.initFil()
     p5.frameRate(20)
     p5.noStroke()
-    this.initRules()
-    this.initHues()
     p5.background(0)
   }
 
   draw = p5 => {
-    this.setRules()
     let currRuleSelect = 0
-    const boxNum = this.state.boxNum
+    const boxNum = this.props.boxNum
     let currHue = 0;
-    let currRow = [...this.state.fil]
+    if (JSON.stringify(lastRules) != JSON.stringify(this.props.rules)) {
+      this.initFil()
+      lastRules = [...this.props.rules]
+    }
+    let currRow = [...this.state.fil];
     let nextRow = []
-    const ux = p5.windowWidth / boxNum
-    const uy = Math.floor(ux)
-    const rowNum = p5.windowHeight / uy
+    const ux = Math.floor(p5.windowWidth / boxNum)+1
+    const uy = ux
+    const rowNum = Math.floor(p5.windowHeight / uy)
     for (let j = 0; j < rowNum; j++) {    
       for (let i = 0; i < boxNum; i++) {
         if (i===0) {
@@ -54,45 +51,46 @@ class P5Sketch extends Component {
         } else {
           currRuleSelect = 4*currRow[i-1]+2*currRow[i]+currRow[i+1]
         }
-        currHue = Math.floor(this.state.hues[currRuleSelect])
+        currHue = Math.floor(this.props.hues[currRuleSelect])
         if (!currRow[i]) {
-          p5.fill((100*(i/(p5.windowWidth/ux))+(100*(j/(p5.windowHeight/uy))+currHue+randHue+100*Math.sin(phase)))%360,this.state.sats[currRuleSelect]-40*(j/(p5.windowHeight/uy)),100)
+          p5.fill((100*(i/(p5.windowWidth/ux))+(100*(j/(p5.windowHeight/uy))+currHue+randHue+100*Math.sin(phase)))%360,this.props.sats[currRuleSelect]-40*(j/(p5.windowHeight/uy)),100)
         } else {
           p5.fill((currHue+randHue)%360,100,0)
         }
-        
-        if (circBool ? p5.dist(p5.mouseX/1.1,p5.mouseY/1.1,ux*i,uy*j) < p5.windowWidth*0.15 : true) {
-          // if (p5.dist(p5.windowWidth*0.5,p5.windowHeight*0.5,ux*i,uy*j) < p5.windowHeight*(p5.mouseY/p5.windowHeight)) {
-          p5.rect(ux*(i-0.5),uy*(j+0.8),ux+2,uy+2)
+       
+        const circRatio = 0.1;
 
+        if ((p5.dist(p5.mouseX/1.1,p5.mouseY/1.1,ux*i,uy*j) < p5.windowWidth*circRatio ?  circBool : false)) {
+          p5.rect(ux*(i-0.5),uy*(j+0.8),ux+5,uy+5)
         }
-          nextRow[i] = this.state.rules[currRuleSelect]
-        
-        
+        nextRow[i] = this.props.rules[currRuleSelect]
       }
-      
       currRow = [...nextRow]
     }
-    this.calcNil()
-    // p5.fill(360*(phase/6.24),100,100)
-    // p5.ellipse(p5.windowWidth*0.1 + 50*Math.cos(phase),p5.windowHeight*0.8+ 50*Math.sin(phase),20,20)
+    this.calcNil()   
     phase+= p5.TWO_PI*0.00511111;
     if (phase > p5.TWO_PI) {
       phase = 0;
     }
   }
 
+  mousePressed = () => { 
+    mouseIsPressed = true;
+  }
+
+  mouseReleased = () => { 
+    mouseIsPressed = false;
+  }
+
   initFil = () => {
     this.setState(
       () => {
         let firstFil = []
-        // const spaceship = [0,0,0,1,0,0,1,1,0,1,1,1,1,1]
-        for (let i = 0; i < this.state.boxNum; i++) {
-          firstFil[i] = 0
-          //  firstFil[i] = Math.floor(Math.random()*2)
-          // firstFil[i] = spaceship[i%spaceship.length]
+        for (let i = 0; i < this.props.boxNum; i++) {
+          // firstFil[i] = 0
+          firstFil[i] = Math.floor(Math.random()*2)
         }
-        firstFil[Math.floor(firstFil.length/2)] = 1
+        // firstFil[Math.floor(firstFil.length/2)] = 1
         return(
           {
             fil: firstFil
@@ -102,53 +100,6 @@ class P5Sketch extends Component {
     )
   }
 
-  initHues = () => {
-    this.setState(
-      () => {
-        let hues = []
-        for (let i = 0; i < 8; i++) {
-         // hues[i] = 0
-         hues[i] = (Math.random()*360)
-          // firstFil[i] = spaceship[i%spaceship.length]
-        }
-        //hues[Math.floor(hues.length/2)] = 1
-        return(
-          {
-            hues: hues
-          }
-        )
-      }
-    )
-  }
-
-  setRules = () => {
-    this.setState(() => {
-      return(
-        {
-          rules:this.props.rules,
-          hues:this.props.hues,
-          sats:this.props.sats
-
-        }
-      )
-    })
-  }
-
-  initRules = () => {
-    this.setState(
-      () => {
-        let ruleArr = []
-        for (let i = 0; i < 8; i++) {
-          ruleArr[i] = Math.floor(Math.random()*2)
-        }
-        return(
-          {
-            rules: ruleArr
-          }
-        )
-      }
-    )
-  }
 
   onWindowResize = (p5) => {
     p5.resizeCanvas(p5.windowWidth,p5.windowHeight)
@@ -156,15 +107,16 @@ class P5Sketch extends Component {
 
   calcNil = () => {
     this.setState( prevState => {
-      const boxNum = this.state.boxNum
+      const boxNum = this.props.boxNum
       const fil = [...prevState.fil]
-      const nil = []
-      const rules = this.state.rules
+      let nil = []
+      const rules = this.props.rules      
       nil[0] = rules[4*fil[boxNum-1]+2*fil[0]+fil[1]]
       nil[boxNum-1] = rules[(4*fil[boxNum-2]+fil[boxNum-1]+fil[0])]
       for (let i = 1; i < fil.length-1; i++) {
         nil[i] = rules[(4*fil[i-1]+2*fil[i]+fil[i+1])]
       }
+      
       return(
         {
           fil: nil
@@ -173,11 +125,9 @@ class P5Sketch extends Component {
     })
   }
 
-
-
   render() {
     return (
-      <Sketch setup={this.setup} draw={this.draw} windowResized={this.onWindowResize}/>
+      <Sketch setup={this.setup} draw={this.draw} windowResized={this.onWindowResize} mousePressed={this.mousePressed} mouseReleased={this.mouseReleased}/>
     )
   }
 
