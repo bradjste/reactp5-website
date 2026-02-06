fdc0823daba4f4554eda2ef67fdbdef14d3de14d
Brad Stevenson <bradjste@gmail.com>
2026-02-05 21:36:14 -0600

    commit

--- remake/src/components/SimpleAudioPlayer.jsx
+++ remake/src/components/SimpleAudioPlayer.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
 
 export default function SimpleAudioPlayer({ playlist }) {
-  return (
-    <div className="simple-player">
-      <h3>Music Player</h3>
-      {playlist && playlist.length > 0 && (
-        <div>
-          {playlist.map((track, idx) => (
-            <div key={idx} className="track-item">
-              <audio controls style={{ width: '100%', marginBottom: '10px' }}>
-                <source src={track.url} type="audio/mpeg" />
-                Your browser does not support the audio element.
-              </audio>
-              <p>
-                <strong>{track.title}</strong> - {track.artist.join(', ')}
-              </p>
-            </div>
-          ))}
+    return (
+        <div className="simple-player">
+            <h3>Music Player</h3>
+            {playlist && playlist.length > 0 && (
+                <div>
+                    {playlist.map((track, idx) => (
+                        <div key={idx} className="track-item">
+                            <audio controls style={{ width: '100%', marginBottom: '10px' }}>
+                                <source src={track.url} type="audio/mpeg" />
+                                Your browser does not support the audio element.
+                            </audio>
+                            <p>
+                                <strong>{track.title}</strong> - {track.artist.join(', ')}
+                            </p>
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  )
+    )
 }
